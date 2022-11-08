134f3c8e130100c535d5b31ee30c7fc3c57a10f5
YoussefCheikhani <76974490+YoussefCheikhani@users.noreply.github.com>
2022-11-08 16:40:10 +0100

    Update DataCleaning Housing Dataset.sql

--- DataCleaning Housing Dataset.sql
+++ DataCleaning Housing Dataset.sql
@@ -8,7 +8,7 @@ From PortfolioProject..NashvilleHousing
 
 -- Replace DATETIME in SaleDate with DATE type
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set SaleDate = CONVERT(DATE,SaleDate)	--Doesn't work all the time?? We do ALTER instead
 
 Alter Table NashvilleHousing
@@ -53,13 +53,13 @@ From PortfolioProject..NashvilleHousing
 Alter Table NashvilleHousing
 Add PropertySplitAddress nvarchar(255)
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
 
 Alter Table NashvilleHousing
 Add PropertyCity nvarchar(255)
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set PropertyCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1,LEN(PropertyAddress))
 
 Select *
@@ -76,20 +76,82 @@ From PortfolioProject..NashvilleHousing
 Alter Table NashvilleHousing
 Add OwnerSplitAddress varchar(255)
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'),3)
 
 Alter Table NashvilleHousing
 Add OwnerCity varchar(255)
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set OwnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'),2)
 
 Alter Table NashvilleHousing
 Add OwnerState varchar(255)
 
-Update NashvilleHousing
+Update PortfolioProject..NashvilleHousing
 Set OwnerState = PARSENAME(REPLACE(OwnerAddress, ',', '.'),1)
 
-Select *
-From NashvilleHousing
+
+--Change Y and N to Yes and No
+
+Select DISTINCT SoldAsVacant
+From PortfolioProject..NashvilleHousing
+
+
+Alter Table PortfolioProject..NashvilleHousing
+Add SoldAsVacantUpdated varchar(255)
+
+
+Update PortfolioProject..NashvilleHousing
+Set SoldAsVacantUpdated = 
+(CASE
+WHEN SoldAsVacant = 'Y' THEN 'Yes'
+WHEN SoldAsVacant = 'N' THEN 'No'
+ELSE SoldAsVacant
+END)
+
+Select DISTINCT SoldAsVacantUpdated
+From PortfolioProject..NashvilleHousing
+
+
+Update PortfolioProject..NashvilleHousing
+Set SoldAsVacant = SoldAsVacantUpdated
+
+Select DISTINCT SoldAsVacant, Count(SoldAsVacant)	--Check
+From PortfolioProject..NashvilleHousing
+Group by SoldAsVacant
+
+Alter Table PortfolioProject..NashvilleHousing
+Drop Column SoldAsVacantUpdated
+
+
+
+--Remove Duplicates
+
+With RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (									--Can also use RANK()
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) rownum                            --when rownum > 1 <==> duplicate row 
+From PortfolioProject..NashvilleHousing
+--Order by rownum DESC
+)
+
+Delete
+From RowNumCTE
+Where rownum>1
+
+
+--Delete unused columns
+
+Select * 
+From PortfolioProject..NashvilleHousing
+
+ALTER TABLE PortfolioProject..NashvilleHousing
+DROP COLUMN PropertyAddress,OwnerAddress,TaxDistrict
